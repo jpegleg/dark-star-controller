b2a059134256254e268de51f13d3d7ee79d00132
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-05-19 15:52:50 -0400

    Create stats.rs
    stats used in disk usage calc

---
+++ src/stats.rs
@@ -0,0 +1,47 @@
+use std::cmp::Ordering;
+use crate::util::shorten_lv;
+
+#[derive(Debug)]
+pub struct Stats {
+    pub filesystem: String,
+    pub size: u64,
+    pub used: u64,
+    pub avail: u64,
+    pub percent: f64,
+    pub mount: String,
+    pub fsid: u64,
+}
+
+impl Stats {
+    pub fn new(fs: &str, size: u64, avail: u64, mount: &str, fsid: u64) -> Stats {
+        let used = size - avail;
+        let percent = used as f64 / size as f64;
+        Stats {
+            filesystem: shorten_lv(fs),
+            size,
+            avail,
+            used,
+            percent: 100.0 * percent,
+            mount: mount.to_string(),
+            fsid,
+        }
+    }
+}
+
+impl Ord for Stats {
+    fn cmp(&self, other: &Stats) -> Ordering {
+        self.filesystem.cmp(&other.filesystem)
+    }
+}
+
+impl PartialOrd for Stats {
+    fn partial_cmp(&self, other: &Stats) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Stats {
+    fn eq(&self, other: &Stats) -> bool {
+        self.filesystem == other.filesystem
+    }
+}
